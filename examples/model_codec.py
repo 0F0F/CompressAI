35714a0fee760bf115c3342054a66baa8f047a55
이성현 <minac@khu.ac.kr>
2020-12-14 13:52:32 +0900

    codec

--- examples/model_codec.py
+++ examples/model_codec.py
@@ -2,6 +2,6 @@ import os
 
 for quality in [1,2,3]:
     for img in range(1, 25):
-        os.system("python model.py encode /data/dataset/test/kodim{img:02d}.png -m mse -q {q} 2 "
+        os.system("python /data/compressai/examples/model.py encode /data/dataset/test/kodim{img:02d}.png -m mse -q {q} 2 "
                   "-o /data/result/{q}_{img:02d}.out".format(img=img,q=quality))
-        os.system("python model.py decode /data/result/{q}_{img:02d}.out -o /data/result/{q}_{img:02d}.jpg".format(img=img,q=quality))
+        os.system("python /data/comprsesai/examples/model.py decode /data/result/{q}_{img:02d}.out -o /data/result/{q}_{img:02d}.jpg".format(img=img,q=quality))
