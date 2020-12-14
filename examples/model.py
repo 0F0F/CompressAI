fc1ada63032ca1456abc6c9597b56f8aac1da956
이성현 <minac@khu.ac.kr>
2020-12-14 13:24:33 +0900

    check input size

--- examples/model.py
+++ examples/model.py
@@ -164,6 +164,7 @@ class ScaleHyperprior_YUV(CompressionModel):
         super().update(force=force)
 
     def compress(self, x):
+        print(x.size())
         x_yuv = self.rgb2yuv(x).squeeze(0)  # shape: [1, 3, w, h]
         x_luma, x_u, x_v = x_yuv.chunk(3, 1)  # y, u, v -> [1, 1, w, h]
         x_chroma = torch.cat((x_u, x_v), dim=1)  # uv -> [1, 2, w, h]
