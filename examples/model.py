5ea35c620dd9d6306d9ad788ae9d9c89c5051a22
이성현 <minac@khu.ac.kr>
2020-12-14 14:16:05 +0900

    yuv2rgb

--- examples/model.py
+++ examples/model.py
@@ -151,9 +151,8 @@ class ScaleHyperprior_YUV(CompressionModel):
 
         x_hat_luma = self.g_s_luma(y_hat_luma)
         x_hat_chroma = self.g_s_chroma(y_hat_chroma)
-
-        x_hat = torch.cat((x_hat_luma, x_hat_chroma), dim=1)
-
+        x_hat_yuv = torch.cat((x_hat_luma, x_hat_chroma), dim=1)
+        x_hat = self.yuv2rgb(x_hat_yuv)
         return {
             "x_hat": x_hat,
             "likelihoods": {"y": y_likelihoods, "z": z_likelihoods},
