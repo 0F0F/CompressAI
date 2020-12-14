64608179403250be3f075503c383ef7fa29ae925
이성현 <minac@khu.ac.kr>
2020-12-14 15:32:19 +0900

    codec path

--- examples/model_codec.py
+++ examples/model_codec.py
@@ -2,7 +2,7 @@ import os
 
 for quality in [1,2,3]:
     for img in range(1, 25):
-        os.system("python /data/compressai/examples/model.py encode /data/dataset/test/kodim{img:02d}.png -m mse -q {q}  "
-                  "-o /data/result/{q}_{img:02d}.out".format(img=img,q=quality))
+        os.system("python /data/compressai/examples/model.py encode /data/dataset/eval/kodim{img:02d}.png -m mse -q {q}  "
+                  "-o /data/result/kodim{img:02d}.out".format(img=img,q=quality))
         os.system("python /data/compressai/examples/model.py decode /data/result/kodim{img:02d}_{q}.out -o /data/result/kodim{img:02d}_{q}.jpg".format(img=img,q=quality))
         os.system("python /data/compressai/examples/model.py decode /data/result/kodim{img:02d}_{q}.out -o /data/result/kodim{img:02d}_{q}.png".format(img=img,q=quality))
